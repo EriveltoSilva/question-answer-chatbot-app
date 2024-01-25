cf745f705e22c896478dbf703b984ef0127983ca
otlevire <eriveltoclenio@gmail.com>
2024-01-25 01:00:43 +0000

    feat: simple and funtional chatbot interaction
    Changes to be committed:
    	modified:   README.md
    	modified:   app.py
    	modified:   requirements.txt

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ This is a simple web chatbot to answer a simples question fast and dinamicly
 # MAIN PROJECT LIBRARIES
 1. langchain
 2. streamlit
+3. openaipip install openai
+
+## Running Process:
+1. Download files
+2. Create virtual Enviroment
+3. Activate Virtual Enviroment 
+4. Install libraries with command: pip install -r requirements.txt
+5. Create .env file for enviroment variables
+6. Put into .env OpenAI API KEY
+7. Run app.py with command: streamlit run app.py
---
+++ app.py
@@ -0,0 +1,26 @@
+from langchain.llms import OpenAI
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_answer(question):
+    llm= OpenAI(model_name="gpt-3.5-turbo-instruct")
+    answer = llm(question)
+    return answer
+
+def get_question():
+    user_question = st.text_input("You:", key="input")
+    return user_question
+
+title = "Grande Sábio"
+st.set_page_config(page_title=title, page_icon=":robot:")
+st.header(title)
+
+user_question = get_question()
+answer = get_answer(user_question)
+
+submit = st.button("Fazer Pergunta")
+if submit:
+    st.subheader("Resposta:")
+    st.write(answer)
